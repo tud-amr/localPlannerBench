fbe29b8a1acff311ccd9705827198d23d79e9139
Max <m.spahn@tudelft.nl>
2021-09-27 16:49:09 +0200

    Generalizes randomized cases to have settings in the config.yaml file

--- infrastructure/expSetup.py
+++ infrastructure/expSetup.py
@@ -3,6 +3,7 @@ import csv
 import gym
 import pointRobot
 import nLinkReacher
+import pandaReacher
 import numpy as np
 
 from obstacle import Obstacle
@@ -12,6 +13,7 @@ from numpyFk import numpyFk
 
 class ExpSetup(object):
     """docstring for Setup"""
+
     def __init__(self, setupFile, randomGoal=False, randomObst=False):
         self._randomGoal = randomGoal
         self._randomObst = randomObst
@@ -19,59 +21,77 @@ class ExpSetup(object):
         self.processFile()
 
     def processFile(self):
-        with open(self._setupFile, 'r') as stream:
+        with open(self._setupFile, "r") as stream:
             self._params = yaml.safe_load(stream)
         if self._randomGoal:
-            self._params['goal'] = np.random.uniform(low=-self._params['n'], high=self._params['n'], size=(2)).tolist()
+            goal = []
+            for i in range(self._params["m"]):
+                goal.append(
+                    np.random.uniform(
+                        low=self._params["randomGoal"]["low"][i],
+                        high=self._params["randomGoal"]["high"][i],
+                    )
+                )
+            self._params["goal"] = goal
         if self._randomObst:
-            self._params['obstacles'] = []
-            for i in range(5):
-                name = 'obst_' + str(i)
-                pos = np.random.uniform(low=-5, high=5, size=(2)).tolist()
-                r = np.random.uniform(low=0.5, high=2.5)
-                obst = {name: name, 'x': pos, 'r': r}
-                self._params['obstacles'].append(obst)
+            self._params["obstacles"] = []
+            for i in range(self._params["randomObstacle"]["nb"]):
+                name = "obst_" + str(i)
+                pos = []
+                for j in range(self._params["m"]):
+                    pos.append(
+                        np.random.uniform(
+                            low=self._params["randomObstacle"]["pos_low"][j],
+                            high=self._params["randomObstacle"]["pos_high"][j],
+                        )
+                    )
+                r = np.random.uniform(
+                    low=self._params["randomObstacle"]["r_low"],
+                    high=self._params["randomObstacle"]["r_high"],
+                )
+                obst = {name: name, "x": pos, "r": r}
+                self._params["obstacles"].append(obst)
 
     def makeEnv(self):
-        return gym.make(self._params['env'], n = self._params['n'], dt=self._params['dt'])
+        return gym.make(self._params["env"], n=self._params["n"], dt=self._params["dt"])
 
     def initialState(self):
-        return self._params['q0'], self._params['q0dot']
+        return self._params["q0"], self._params["q0dot"]
 
     def goal(self):
-        return self._params['goal']
+        return self._params["goal"]
 
     def obstacles(self):
         self._obsts = []
-        for o in self._params['obstacles']:
-            self._obsts.append(Obstacle(o['x'], o['r']))
-        return self._obsts 
+        for o in self._params["obstacles"]:
+            self._obsts.append(Obstacle(o["x"], o["r"]))
+        return self._obsts
 
     def n(self):
-        return self._params['n']
+        return self._params["n"]
 
     def T(self):
-        return self._params['T']
+        return self._params["T"]
 
     def lowerLimits(self):
-        return self._params['limits']['lower']
+        return self._params["limits"]["lower"]
 
     def upperLimits(self):
-        return self._params['limits']['upper']
+        return self._params["limits"]["upper"]
 
     def checkFeasibility(self):
         for o in self._obsts:
-            for i in range(1, self._params['n'] + 1):
-                fk = numpyFk(self._params['q0'], i)[0:2]
+            for i in range(1, self._params["n"] + 1):
+                fk = numpyFk(self._params["q0"], i)[0:2]
                 dist_initState = np.linalg.norm(np.array(o.x()) - fk)
                 if dist_initState < o.r():
                     print("Infeasible initial configuration")
                     return False
-            dist_goal = np.linalg.norm(np.array(o.x()) - np.array(self._params['goal']) )
+            dist_goal = np.linalg.norm(np.array(o.x()) - np.array(self._params["goal"]))
             if dist_goal < o.r():
                 print("Goal in collision")
                 return False
-            if np.linalg.norm(np.array(self._params['goal'])) > self._params['n']:
+            if np.linalg.norm(np.array(self._params["goal"])) > self._params["n"]:
                 print("Goal unreachible")
                 return False
         return True
@@ -81,13 +101,13 @@ class ExpSetup(object):
         goalFilename = folderPath + "/goal.csv"
         initStateFilename = folderPath + "/initState.csv"
         obstFile = folderPath + "/obst"
-        with open(expFilename, 'w') as file:
+        with open(expFilename, "w") as file:
             yaml.dump(self._params, file)
         for i, o in enumerate(self._obsts):
             o.toCSV(obstFile + "_" + str(i) + ".csv")
-        with open(goalFilename, 'w') as file:
-            csv_writer = csv.writer(file, delimiter=',')
-            csv_writer.writerow(self._params['goal'])
-        with open(initStateFilename, 'w') as file:
-            csv_writer = csv.writer(file, delimiter=',')
-            csv_writer.writerow(self._params['q0'])
+        with open(goalFilename, "w") as file:
+            csv_writer = csv.writer(file, delimiter=",")
+            csv_writer.writerow(self._params["goal"])
+        with open(initStateFilename, "w") as file:
+            csv_writer = csv.writer(file, delimiter=",")
+            csv_writer.writerow(self._params["q0"])
