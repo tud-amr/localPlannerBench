244a3a86b56f69c465c90162c0074b626d346233
Max <m.spahn@tudelft.nl>
2022-02-01 15:59:29 +0100

    Adds comparision between mpc and fabric for boxer robot in simulation.

--- plannerbenchmark/planner/mpc/makeSolver.py
+++ plannerbenchmark/planner/mpc/makeSolver.py
@@ -1,13 +1,13 @@
 from robotmpcs.models.pointRobotMpcModel import PointRobotMpcModel
 from robotmpcs.models.planarArmMpcModel import PlanarArmMpcModel
-#from robotmpcs.boxerMpcModel import BoxerMpcModel
+from robotmpcs.models.boxerMpcModel import BoxerMpcModel
 from robotmpcs.models.pandaMpcModel import PandaMpcModel
 
 
 def createSolver():
-    robotType = "panda"
+    robotType = "boxer"
     debug = False
-    slack = True
+    slack = False
     dt = 0.1
     N = 30
     if robotType == 'planarArm':
@@ -19,13 +19,16 @@ def createSolver():
     elif robotType == 'panda':
         n = 7
         mpcModel = PandaMpcModel(3, N)
+    elif robotType == 'boxer':
+        n = 2
+        mpcModel = BoxerMpcModel(N)
     else:
         print(f"Solver for robot type {robotType} not available yet")
         
     if slack:
         mpcModel.setSlack()
     mpcModel.setDt(dt)
-    mpcModel.setObstacles(5, 3, inCostFunction=False)
+    mpcModel.setObstacles(5, mpcModel._m, inCostFunction=False)
     mpcModel.setModel()
     mpcModel.setCodeoptions(debug=debug)
     location = "./solverCollection/"
