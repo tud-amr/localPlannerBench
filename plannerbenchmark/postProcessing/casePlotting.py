e1febf2edf8655e4c017bfadd3ac06198e481894
Max Spahn <m.spahn@tudelft.nl>
2022-03-10 15:43:46 +0100

    Adds docstrings for casePlotting.

--- plannerbenchmark/postProcessing/casePlotting.py
+++ plannerbenchmark/postProcessing/casePlotting.py
@@ -7,13 +7,21 @@ pkg_path = os.path.dirname(__file__) + '/../postProcessing/'
 
 class CasePlotting(object):
 
-    """Docstring for CasePlotting. """
+    """Wrapper to direct to the correct gnuplot script for plotting the results
+    of the experiment to an appropriate format."""
 
-    def __init__(self, folder):
-        self._folder = folder
-        self._experiment = Experiment(self._folder + "/exp.yaml")
+    def __init__(self, folder: str):
+        self._folder:       str = folder
+        self._experiment:   Experiment = Experiment(self._folder + "/exp.yaml")
 
-    def plot(self):
+    def plot(self) -> None:
+        """Call the correct gnuplot script based on the robot type.
+
+        The gnuplot scripts are called using subprocess.Popen to avoid
+        additional libraries. Depending on the robot type, the gnuplot scripts
+        take a different number of arguments. Output from the gnuplot scripts
+        is passed to the subprocess.PIPE.
+        """
         curPath = os.path.dirname(os.path.abspath(__file__)) + "/"
         curPath = pkg_path
         if self._experiment.robotType() == "planarArm":
