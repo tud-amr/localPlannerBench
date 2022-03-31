aed11856bb754f09ccfa44ad67b109f3e8c2e447
Max Spahn <m.spahn@tudelft.nl>
2022-03-31 16:12:53 +0200

    Adds simple local tests for point mass.

--- plannerbenchmark/tests/test_local.py
+++ plannerbenchmark/tests/test_local.py
@@ -5,6 +5,6 @@ from plannerbenchmark.tests.test_functions import *
 
 def test_all_cases():
     absPath = os.path.dirname(os.path.abspath(__file__))
-    caseFolders = absPath + '/cases/'
+    caseFolders = absPath + '/local_cases/'
     for case in os.listdir(caseFolders):
-        run_integration_test_case('cases/' + case)
+        run_integration_test_case('local_cases/' + case)
