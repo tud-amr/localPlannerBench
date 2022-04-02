363d74dbb725fb26b0086859a979fb7f20068d46
Max Spahn <m.spahn@tudelft.nl>
2022-04-02 11:44:13 +0200

    Adds try clause around planner imports.

--- plannerbenchmark/planner/__init__.py
+++ plannerbenchmark/planner/__init__.py
@@ -1,3 +1,9 @@
-from plannerbenchmark.planner.fabricPlanner import FabricPlanner
-from plannerbenchmark.planner.mpcPlanner import MPCPlanner
+try:
+    from plannerbenchmark.planner.fabricPlanner import FabricPlanner
+except ModuleNotFoundError as e:
+    print(f"Module fabricPlanner not found: {e}")
+try:
+    from plannerbenchmark.planner.mpcPlanner import MPCPlanner
+except ModuleNotFoundError as e:
+    print(f"Module mpcPlanner not found: {e}")
 from plannerbenchmark.planner.pdPlanner import PDPlanner
